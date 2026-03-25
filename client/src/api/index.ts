82f1e1e2aa82528608287e7012af569b645635a9
Сахарок <taturroma740@gmail.com>
2026-03-25 14:43:36 +0300

    Delete client directory

--- client/src/api/index.ts
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-// Создаем экземпляр с базовыми настройками
-const api = axios.create({
-    baseURL: 'http://localhost:5000/api', // Адрес нашего сервера
-    withCredentials: true // РАЗРЕШАЕМ КУКИ (Самое важное!)
-});
-
-export default api;
